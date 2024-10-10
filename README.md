dd58bc8d03607ea1ae339fe8e09b1053b7a72581
Rutul Bhosale <rutulbhosale@Rutuls-MacBook-Air.local>
2024-10-10 11:11:27 +0530

    Updated frontend components and added new files

--- README.md
+++
@@ -1,1 +0,0 @@
-# Food_Ordering_webapp
--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -1,21 +1,24 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import { Button } from './ui/button';
+import UsernameMenu from './UsernameMenu';
 
 export default function MainNav() {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
 
   return (
-    <div>
-      <Button 
-        variant="ghost" 
-        className='font-bold hover:text-orange-500 hover:bg-white'
-        onClick={async () => {
-          console.log("Login button clicked"); // Log when the button is clicked
-          await loginWithRedirect(); // Redirect to Auth0 login
+    <span className='flex space-x-2 items-center'>
+      {isAuthenticated ? (<UsernameMenu />
+      ) : (<Button 
+      variant="ghost" 
+      className='font-bold hover:text-orange-500 hover:bg-white'
+      onClick={async () => {
+        console.log("Login button clicked"); // Log when the button is clicked
+        await loginWithRedirect(); // Redirect to Auth0 login
         }}
       >
-        Log In
-      </Button>
-    </div>
+      Log In
+    </Button>
+  )}
+    </span>
   );
 }
